442d79c3ee31c46b60bb390fa2652d09ea16a71c
Tzu-Ti <a2699560@gmail.com>
2019-11-16 18:05:37 +0800

    monitor.html bottom information

--- script/formal.js
+++ script/formal.js
@@ -5,7 +5,7 @@ function enterSmallDiv() {
 	$(this).find("#popDivImage2").stop();
 	
 	$(this).find(".popDivName").animate({
-		"top": "20%"
+		"top": "15%"
 	}, 500);
 	$(this).find("#popDivImage1").animate({
 		"width": "toggle"
@@ -27,34 +27,57 @@ function leaveSmallDiv() {
 	});
 	$(this).find("#popDivImage2").fadeToggle(500);
 };
+var change = 0;
+function popDiv() {
+    $(".popDiv").slideToggle(500);
+    $(".popDivShadow").slideToggle(500);
+    if (change == 0) {
+        $(".topMenu").animate({
+            "width": "3px",
+            "height": "20px",
+            "border-width": "0px 3px",
+            "padding": "0 8px",
+        });
+        change = 1;
+    }
+    else {
+        $(".topMenu").animate({
+            "width": "30px",
+            "height": "3px",
+            "border-width": "3px 0px",
+            "padding": "8px 0",
+        });
+        change = 0;
+    }
+}
 
 $(function() {
     $(".topLogoDiv").bind("click", function() {
         location.href = "index.html";
     });
-    var change = 0;
-    $(".topMenu").bind("click", function() {
-        $(".popDiv").slideToggle(500);
-		$(".popDivShadow").slideToggle(500);
-        if(change == 0) {
-            $(".topMenu").animate({
-                "width": "3px",
-                "height": "20px",
-                "border-width": "0px 3px",
-                "padding": "0 8px",
-            });
-            change = 1;
-        }
-        else {
-            $(".topMenu").animate({
-                "width": "30px",
-                "height": "3px",
-                "border-width": "3px 0px",
-                "padding": "8px 0",
-            });
-            change = 0;
+    $(".popDivSmall").bind("click", function() {
+        var popId = $(this).attr("id");
+        switch(popId) {
+            case "popDiv1":
+                location.href = "monitor.html";
+                break;
+            case "popDiv2":
+                location.href = "http://google.com";
+                break;
+            case "popDiv3":
+                location.href = "http://google.com";
+                break;
+            case "popDiv4":
+                location.href = "http://google.com";
+                break;
+            case "popDiv5":
+                location.href = "http://google.com";
+                break;
         }
     });
+    
+    $(".topMenu").bind("click", popDiv);
+    $(".popDivShadow").bind("click", popDiv);
 });
 $(function() {
 	$(".popDivSmall").mouseenter(enterSmallDiv);
